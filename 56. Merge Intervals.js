83fc770f947a9af927d6a8e4bfcab18daa676f3e
Joey Yu <joeyyu.van@gmail.com>
2021-02-17 17:51:14 -0800

    56. Merge Intervals

---
+++ 56. Merge Intervals.js
@@ -0,0 +1,12 @@
+var merge = function (intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  for (let i = 1; i < intervals.length; ++i) {
+    if (intervals[i][0] <= intervals[i - 1][1]) {
+      intervals[i - 1][1] = Math.max(intervals[i - 1][1], intervals[i][1]);
+      intervals.splice(i, 1);
+      --i;
+    }
+  }
+
+  return intervals;
+};
