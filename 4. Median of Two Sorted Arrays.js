928392069efcb2a8b95653f6a443ce1c440dd1d7
Joey Yu <joeyyu.van@gmail.com>
2021-02-22 19:53:28 -0800

    4. Median of Two Sorted Arrays

---
+++ 4. Median of Two Sorted Arrays.js
@@ -0,0 +1,9 @@
+var findMedianSortedArrays = function (nums1, nums2) {
+  let nums = nums1.concat(nums2);
+  nums.sort((a, b) => a - b);
+  if (nums.length % 2 === 0) {
+    return (nums[nums.length / 2] + nums[nums.length / 2 - 1]) / 2;
+  }
+
+  return nums[parseInt(nums.length / 2)];
+};
