a59b59d03dac2bdb7fbc565dedff71f28be107c7
Joey Yu <joeyyu.van@gmail.com>
2021-02-28 15:07:44 -0800

    Merge two LRU Cache solutions

--- 146. LRU Cache.js
+++ 146. LRU Cache.js
@@ -33,3 +33,31 @@ LRUCache.prototype.put = function (key, value) {
 
   this.cache.push([key, value]);
 };
+
+/**********************************************************************************************/
+/**
+ * Another solution using Map()
+ * Copied from: https://leetcode.com/problems/lru-cache/discuss/399146/Clean-JavaScript-solution
+ * It works since JavaScript Map is an orderedDict.
+ */
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.cache = new Map();
+};
+
+LRUCache.prototype.get = function (key) {
+  if (!this.cache.has(key)) return -1;
+
+  let value = this.cache.get(key);
+  this.cache.delete(key);
+  this.cache.set(key, value);
+  return value;
+};
+
+LRUCache.prototype.put = function (key, value) {
+  if (this.cache.has(key)) this.cache.delete(key);
+  this.cache.set(key, value);
+  if (this.cache.size > this.capacity) {
+    this.cache.delete(this.cache.keys().next().value);
+  }
+};
