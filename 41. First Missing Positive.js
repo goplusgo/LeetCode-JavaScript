c4bc3e4c45c0cb98c19b4bbcb66de15ca6217ec3
Joey Yu <joeyyu.van@gmail.com>
2021-02-28 12:24:27 -0800

    41. First Missing Positive

---
+++ 41. First Missing Positive.js
@@ -0,0 +1,19 @@
+var firstMissingPositive = function (nums) {
+  for (let i = 0; i < nums.length; ++i) {
+    while (
+      nums[i] !== i + 1 &&
+      nums[i] >= 1 &&
+      nums[i] <= nums.length &&
+      nums[nums[i] - 1] != nums[i]
+    ) {
+      // swap two elements
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+    }
+  }
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] != i + 1) {
+      return i + 1;
+    }
+  }
+  return nums.length + 1;
+};
